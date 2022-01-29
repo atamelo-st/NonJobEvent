1a491d898a9434720fd60f1ba43ad7b53022ef40
Andrey Tamelo <atamelo@servicetitan.com>
2022-01-29 04:43:20 -0800

    adding some experiments with Try

---
+++ src/NonJobEvent/Try.cs
@@ -0,0 +1,69 @@
+﻿namespace Experiment;
+
+public abstract record Try
+{
+    public static Success<TData> SucceedWith<TData>(TData data) => new(data);
+
+    public static Failure<TFailure> FailWith<TFailure>(TFailure failure) where TFailure : Try.Failure
+        => new(failure);
+
+    public abstract record Of<TExpectedData>
+    {
+        public static Failure<TFailure> FailWith<TFailure>(TFailure failure) where TFailure : Try.Failure
+            => new(failure);
+
+        public sealed record Failure<TFailure>(TFailure FailedWith) : Try.Of<TExpectedData>, Failure
+            where TFailure : Try.Failure
+        {
+            public static implicit operator Try.Of<TExpectedData>.Failure<TFailure>(Try.Failure<TFailure> failure)
+                => new(failure.FailedWith);
+        }
+    }
+
+    public sealed record Success<TData>(TData Data) : Try.Of<TData>, Success;
+
+    public sealed record Failure<TFailure>(TFailure FailedWith) : Try.Of<TFailure>, Failure
+        where TFailure : Try.Failure;
+
+    public interface Success
+    {
+    }
+
+    public interface Failure
+    {
+    }
+}
+
+
+public class Test
+{
+    public void Method()
+    {
+        Try.Success r0 = Try.SucceedWith(1);
+        Try.Of<string> r1 = Try.SucceedWith("Hello");
+        Try.Of<string> r2 = Try.Of<string>.FailWith(Http.NotFound(4)); // Result<string>.OfFailure(new Http.NotFound(3));
+        Try.Of<string>.Failure<Http.Failure.NotFound> r3 = Try.FailWith(Http.NotFound(4));
+        Try.Of<string> r4 = r3;
+        Try.Of<string> r5 = Try.Of<string>.FailWith(Http.NotFound(4));
+
+        string s = r2 switch
+        {
+            Try.Success<string> success => "",
+            Try.Success => "",
+            Try.Of<string>.Failure<Http.Failure.NotFound> notFound => $"{notFound.FailedWith.Code}",
+            // Try.Failure<Http.Failure.NotFound> notFounf => "nf",
+            Try.Failure => "failure",
+            _ => throw new NotImplementedException(),
+        };
+    }
+
+    public abstract record Http : Try.Failure
+    {
+        public static Failure.NotFound NotFound(int code) => new(code);
+
+        public abstract record Failure : Try.Failure
+        {
+            public record NotFound(int Code) : Failure;
+        }
+    }
+}
